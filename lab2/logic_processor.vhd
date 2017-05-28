5f7a25ff7d59fc2037c99559b2ef9132314d0ebe
Joanna Chen <joannachen.x@gmail.com>
2017-05-28 15:56:08 -0400

    Add logic processor

---
+++ lab2/logic_processor.vhd
@@ -0,0 +1,30 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity logic_processor is
+port(
+	hex_A, hex_B			: in std_logic_vector(3 downto 0);
+	pb							: in std_logic_vector(3 downto 0);
+	logic_func				: out std_logic_vector(3 downto 0)
+	);
+end entity logic_processor;
+
+architecture logic of logic_processor is
+
+signal sum 					: std_logic_vector(3 downto 0);
+
+begin
+
+sum <= std_logic_vector(unsigned(hex_A) + unsigned(hex_B));
+
+with pb select
+logic_func <= hex_A AND hex_B when "1110",
+				  hex_A OR hex_B when "1101",
+				  hex_A XOR hex_B when "1011",
+				  sum when "0111",
+				  null when others;
+				  --CASE when "1111"; -- leds are turned off
+				  --DO SOMETHING WHEN other; -- seven seg shouild show 8, all leds should turn on
+
+end logic;
